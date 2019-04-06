9bca241ee5d3d2416089c5f63fe48c9de6c2e671
Dan Tong <dan@Dan-Tapi-Magic.local>
2019-04-06 13:53:44 +1300

    Add ShowRecipient view

---
+++ assets/src/components/form/InfoField.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}</label>
+    <transition name="fade">
+      <div v-if="value" class="control">
+        <p class="text">{{ value }}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    show: () => true
+  },
+  props: ["label", "value"]
+};
+</script>
+
+<style lang="scss" scoped>
+.field {
+  margin-top: 1.5rem;
+  .text {
+    margin-left: 0.2rem;
+  }
+}
+</style>
