1c36302a34b31838fef389245e0204f82ca557d3
Dan Tong <dan@dtong.co>
2019-09-01 13:16:52 +1200

    Rename HoldStatusLabel to HoldStatusTag

--- assets/src/components/form/HoldStatusTag.vue
+++ assets/src/components/form/HoldStatusTag.vue
@@ -12,7 +12,7 @@ import { ActiveRecipientModule } from "@/store/modules/active-recipient";
 import { sortBy } from "lodash";
 
 @Component
-export default class HoldStatusLabel extends Vue {
+export default class HoldStatusTag extends Vue {
   @Prop() readonly status!: string;
 
   get labelType() {
