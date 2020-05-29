e1a2e40043f1a8ca8acadf78110d69ed187d24f8
Dan Tong <dan@dtong.co>
2020-05-29 14:56:58 +1200

    Configure eslint and compat

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
+    "plugin:compat/recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
