0967510af23c06699ad7c48e5c46d3e4ac9cd22e
Dan Tong <dan@dtong.nz>
2020-07-13 16:34:53 +1200

    Make contact name optional
    There may only be an email/phone number without a name

--- priv/repo/migrations/20200704035134_create_contacts.exs
+++ priv/repo/migrations/20200704035134_create_contacts.exs
@@ -5,7 +5,7 @@ defmodule Kaibosh.Repo.Migrations.CreateContacts do
     create table(:contacts) do
       add :recipient_id, references(:recipients, on_delete: :delete_all), null: false
 
-      add :name, :string, null: false
+      add :name, :string
       add :email, :string
       add :phone_landline, :string
       add :phone_mobile, :string
