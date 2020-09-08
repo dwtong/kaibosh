5e801d82bd697e001d1cb7dc3ef7d72f177a7d29
Dan Tong <dan@dtong.nz>
2020-09-09 10:54:01 +1200

    Don't include sentry in test/dev envs

--- config/config.exs
+++ config/config.exs
@@ -40,4 +40,6 @@ config :sentry,
 #
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+config :sentry, included_environments: ["staging", "prod"]
+
 import_config "#{Mix.env()}.exs"
