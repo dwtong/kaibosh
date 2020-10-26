2568d614e57336d90374cff191d4a77ca240556a
Dan Tong <dan@dtong.nz>
2020-10-26 13:58:39 +1300

    Toggle email notifications in user settings

--- assets/src/components/users/NotificationSettings.vue
+++ assets/src/components/users/NotificationSettings.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <NotificationCheckbox
+    <Checkbox
       v-for="notification in notifications"
       :key="notification.id"
-      :id="notification.id"
       :label="notification.name"
       :value="notification.enabled"
+      @input="toggleValue(notification.id, $event)"
     />
   </div>
 </template>
@@ -13,11 +13,11 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import NotificationCheckbox from "@/components/ui/NotificationCheckbox.vue";
+import Checkbox from "@/components/ui/Checkbox.vue";
 import App from "@/store/modules/app";
 import UserNotificationService from "@/services/user-notification-service"
 
-@Component({ components: { NotificationCheckbox } })
+@Component({ components: { Checkbox } })
 export default class NotificationSettings extends Vue {
   notifications = []
   isEnabled(baseId: string) {
@@ -27,6 +27,14 @@ export default class NotificationSettings extends Vue {
 
   async created() {
     this.notifications = await UserNotificationService.list()
+  }
+
+  toggleValue(id: string, value: boolean) {
+    if (value) {
+      UserNotificationService.create(id);
+    } else {
+      UserNotificationService.delete(id);
+    }
   }
 }
 </script>
