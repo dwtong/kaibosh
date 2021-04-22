ad76deee25a7a82f63e45e882db5309e6ad15e4c
Dan Tong <dan@dtong.nz>
2021-04-22 17:11:54 +1200

    Create Message component

---
+++ assets/src/components/ui/Message.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="message" :class="type">
+    <div class="message-header">
+      <p>{{ title }}</p>
+      <button class="delete" aria-label="delete"></button>
+    </div>
+    <div class="message-body">
+      <slot />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    type: {
+      type: String,
+      default: "is-info"
+    }
+  }
+});
+</script>
--- assets/src/components/ui/RecipientMessageBox.vue
+++ assets/src/components/ui/RecipientMessageBox.vue
@@ -1,23 +1,17 @@
 <template>
   <transition name="fade">
-    <b-message v-if="status === 'pending'" title="Pending" type="is-info" has-icon aria-close-label="Close message">
+    <Message v-if="status === 'pending'" title="Pending" type="is-info" has-icon aria-close-label="Close message">
       <p>
         {{ name }} is currently pending. For a recipient to become active, they must:<br /><br />
         &nbsp;&nbsp;&nbsp;&nbsp;1. Have their start date set to today or earlier.<br />
         &nbsp;&nbsp;&nbsp;&nbsp;2. Have all 'Onboarding' items checked off.<br />
         &nbsp;&nbsp;&nbsp;&nbsp;3. Have at least one sorting session set.<br />
       </p>
-    </b-message>
+    </Message>
 
-    <b-message
-      v-if="status === 'archived'"
-      title="Archived"
-      type="is-warning"
-      has-icon
-      aria-close-label="Close message"
-    >
+    <Message v-if="status === 'archived'" title="Archived" type="is-warning" has-icon aria-close-label="Close message">
       <p>{{ name }} is currently archived. <a @click="reactivate">Click here</a> to reactivate them.</p>
-    </b-message>
+    </Message>
   </transition>
 </template>
 
@@ -25,8 +19,12 @@
 import { defineComponent } from "vue";
 import ActiveRecipient from "@/store/modules/active-recipient";
 import App from "@/store/modules/app";
+import Message from "@/components/ui/Message.vue";
 
 export default defineComponent({
+  components: {
+    Message
+  },
   props: {
     status: {
       type: String,
