3c8ae8bfa9fc81cd4cfb2e40876147cc4dd735fc
Dan Tong <dan@dtong.nz>
2023-07-02 11:55:24 +1200

    Add LoadingSpinner component

---
+++ client/src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps<{ isActive: boolean }>()
+</script>
+
+<template>
+  <div class="loader-wrapper" :class="{ 'is-active': props.isActive }">
+    <div class="loader is-loading"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-wrapper {
+  background: #fff;
+  opacity: 0;
+  z-index: -1;
+  transition: opacity 0.3s;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+
+  .loader {
+    height: 40px;
+    width: 40px;
+  }
+
+  &.is-active {
+    display: flex;
+    opacity: 1;
+    z-index: 1;
+  }
+}
+
+.is-loading {
+  position: relative;
+}
+</style>
